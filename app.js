c9caacd05afe03827bfdb73a743d5a9262c08698
HAROLPERIODICO <haroldiazmelendez@gmail.com>
2025-05-31 10:10:33 -0500

    primer commit echo en git

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Server  peticion recibida')
+    res.send('<h1>Hola Harol Díaz Meléndez</h1>')
+})
+    
+app.listen(4000, () => {
+    console.log('Server escuchando 4000')
+})
